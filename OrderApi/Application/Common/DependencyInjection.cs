4fcaa5a2c9f0193adfca1007f151664a61a88099
Talha <talha.sahin@accenture.com>
2025-02-08 16:43:23 +0300

    Registered Services

--- OrderApi/Application/Common/DependencyInjection.cs
+++ OrderApi/Application/Common/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using OrderApi.Application.Interfaces;
 using OrderApi.Application.Mapping;
+using OrderApi.Application.Services;
 
 namespace OrderApi.Application.Common;
 
@@ -8,6 +10,14 @@ public static class DependencyInjection
     {
         // Register AutoMapper
         services.AddAutoMapper(typeof(MappingProfile));
+        // Register Services
+        services.AddScoped(typeof(IBaseService<>), typeof(BaseService<,>));
+
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IUserService, UserService>();
+        
         return services;
     }
 }
